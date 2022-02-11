08195a0deb8081f326781958d3dc4ff201300758
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-12 00:50:33 +0600

    Time: 120 ms (27.17%), Space: 14 MB (74.47%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -1,25 +1,14 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        table = {}
-        for i in nums1:
-            if i not in table:
-                table[i] = {1}
-                
-        for i in nums2:
-            if i not in table:
-                table[i] = {2}
-            else:
-                table[i].add(2)
+        set1 = set(nums1)
+        set2 = set(nums2)
+        set3 = set(nums3)
         
-        for i in nums3:
-            if i not in table:
-                table[i] = {3}
-            else:
-                table[i].add(3)
-
-        li = []
-        for i in table:
-            if len(table[i]) >= 2:
-                li.append(i)
-        return li
-            
+        # Set Intersection
+        ints1 = set1 & set2
+        ints2 = set1 & set3
+        ints3 = set2 & set3
+        
+        # Set union
+        uni = ints1 | ints2 | ints3
+        return uni
