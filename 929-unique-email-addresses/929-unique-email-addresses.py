23f518dcaacbb5f2a2cd3ff8c77be8a2134ffb49
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-13 16:01:56 +0600

    Time: 62 ms (67.41%), Space: 13.9 MB (99.30%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.py
+++ 929-unique-email-addresses/929-unique-email-addresses.py
@@ -1,25 +1,10 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        table = []
+        uniqueEmail = set()
         for email in emails:
-            em = ""
-            plus = False
-            for i in range(len(email)):
-                if email[i] == ".":
-                    continue
-                elif email[i] == "+":
-                    plus = True
-                elif email[i] == "@":
-                    em += email[i:]
-                    break
-                elif plus == True:
-                    continue
-                else:
-                    em += email[i]
+            local, domain = email.split("@")
+            name = local.split("+")[0].replace(".", "")
+            uniqueEmail.add(name + "@" + domain)
                     
-            if em not in table:
-                table.append(em)
-                    
-        # print(table)
-        l = len(table)
+        l = len(uniqueEmail)
         return l
