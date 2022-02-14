a0ae81be01bee2c12107efd5f7a89d781a068c6e
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-14 21:51:16 +0600

    Time: 104 ms (26.88%), Space: 14.2 MB (67.64%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        table = {}
-        for i in magazine:
-            if i not in table:
-                table[i] = 1
-            else:
-                table[i] += 1
-                
-        for i in ransomNote:
-            if i not in table or table[i] == 0:
+        d1={}
+        d2={}
+        for x in ransomNote:
+            d1[x] = d1.get(x,0)+1
+
+        for x in magazine:
+            d2[x] = d2.get(x,0)+1
+            
+        for x in d1:
+            if x not in d2 or d1[x] > d2[x]:
                 return False
-            else:
-                table[i] -= 1
+            
         return True
