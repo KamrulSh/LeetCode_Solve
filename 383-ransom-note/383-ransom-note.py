18a4bde50de490d757a89b50ba168cd5ac54cbf0
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-14 22:10:07 +0600

    Time: 68 ms (69.23%), Space: 14.2 MB (67.64%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,5 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        d1={}
-        d2={}
-        for x in ransomNote:
-            d1[x] = d1.get(x,0)+1
-
-        for x in magazine:
-            d2[x] = d2.get(x,0)+1
-            
-        for x in d1:
-            if x not in d2 or d1[x] > d2[x]:
-                return False
-            
-        return True
+        ransomNote_stat = collections.Counter(ransomNote)
+        magazine_stat = collections.Counter(magazine)
+        return magazine_stat >= ransomNote_stat
