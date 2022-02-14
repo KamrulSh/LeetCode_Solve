72ddc7b7eb5d2b7336a77acc75c2984fe67c2377
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-14 21:05:26 +0600

    Time: 68 ms (69.23%), Space: 14.3 MB (67.64%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        table = {}
+        for i in magazine:
+            if i not in table:
+                table[i] = 1
+            else:
+                table[i] += 1
+                
+        for i in ransomNote:
+            if i not in table or table[i] == 0:
+                return False
+            else:
+                table[i] -= 1
+        return True
