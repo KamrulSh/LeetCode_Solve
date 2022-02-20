0295686c2d29e7530c4ee7d2e8a5b6a1fd5b7a3c
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-20 16:50:05 +0600

    Time: 43 ms (50.10%), Space: 14 MB (73.20%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -1,19 +1,22 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
-        table1 = {}
-        table2 = {}
-        for i in word1:
-            if i not in table1:
-                table1[i] = 1
-            else:
-                table1[i] += 1
+#         table1 = {}
+#         table2 = {}
+#         for i in word1:
+#             if i not in table1:
+#                 table1[i] = 1
+#             else:
+#                 table1[i] += 1
         
-        for i in word2:
-            if i not in table2:
-                table2[i] = 1
-            else:
-                table2[i] += 1
+#         for i in word2:
+#             if i not in table2:
+#                 table2[i] = 1
+#             else:
+#                 table2[i] += 1
         
+        
+        table1 = Counter(word1)
+        table2 = Counter(word2)
         for item in table2:
             if item not in table1:
                 table1[item] = table2[item]
