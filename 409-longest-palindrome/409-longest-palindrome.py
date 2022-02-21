2c6092e42558134f7baddeb76a1c53e3d9ad10d7
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-02-22 01:05:35 +0600

    Time: 55 ms (28.63%), Space: 13.9 MB (68.34%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        table_set = set()
+        for i in s:
+            if i not in table_set:
+                table_set.add(i)
+            else:
+                table_set.remove(i)
+                
+        if len(table_set) > 0:
+            length = len(s) - len(table_set) + 1
+        else: 
+            length = len(s)
+        return length
+    
+
+#         chars = set()
+#         res = 0
+#         for i, char in enumerate(s):
+#             if char in chars:
+#                 res += 2
+#                 chars.remove(char)
+#             else:
+#                 chars.add(char)
+            
+#         if res < len(s):
+#             res += 1
+        
+#         return res
