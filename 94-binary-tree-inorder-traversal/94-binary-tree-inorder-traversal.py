52b258e43e834e2c5fa95ecf7a44f0cd2bff9d86
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-03-11 00:35:59 +0600

    Time: 31 ms (87.34%), Space: 14 MB (32.41%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         inorder = []
