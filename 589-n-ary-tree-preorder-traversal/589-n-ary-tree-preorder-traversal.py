9d52e3cd185dd641a8e72f039a9cbc593c9ee83f
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2022-03-20 18:47:33 +0600

    Time: 63 ms (65.80%), Space: 16 MB (81.33%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        output = []
+        while stack:
+            popped = stack.pop()
+            output.append(popped.val)
+            stack.extend(popped.children[::-1])
+        return output
