d195dfbfbcc8a993b9606a25a5d00007a0b82ac4
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2023-05-30 11:35:55 +0600

    Time: 60 ms (51.2%), Space: 16.3 MB (53.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        prev = 0
+        sum = 0
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        
+        for sym in s[::-1]:
+            if prev > roman[sym]:
+                sum -= roman[sym]
+            else:
+                sum += roman[sym]
+            prev = roman[sym]
+        return sum
